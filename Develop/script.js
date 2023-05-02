25278216ce256597ba3f773cb2b1e87d99eac94a
Cameron French <cameron.french92@gmail.com>
2023-05-02 00:10:06 -0400

    added prompts to gather criteria for password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword ()
+function generatePassword () {
   var length = prompt("How many characters will your password be? Enter a number between 8 and 128");
-  var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
-  var number = "0123456789"; 
-  var special = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
+  var lowerLetters = prompt("Would you like like to include lowercase letters in your password?");
+
+  var upperLetters = prompt("Would you like like to include uppercase letters in your password?");
+
+  var numeric = prompt("Would you like like to include numbers in your password?");
+
+  var specialChar = prompt("Would you like like to include special characters in your password?");
+
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+
+  var number = "0123456789"; 
+
+  var special = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"; 
+
+  var random = Math.floor(Math.random() * lowercase.length);
+
+  // for (var i = 0; i < )
+
+  return ""; 
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -18,7 +35,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+ 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
